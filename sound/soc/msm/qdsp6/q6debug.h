a85c01b20e4f1d920e6f4d473bdf8f1a132b8228
bilalliberty <dominosliberty@gmail.com>
2014-03-04 04:09:16 +0300

    update defconfig

--- sound/soc/msm/qdsp6/q6debug.h
+++
@@ -1,9 +0,0 @@
-#include <mach/subsystem_restart.h>
-
-#ifdef CONFIG_SND_HTC_Q6_NOBUG
-#define HTC_Q6_BUG()
-#else
-#define HTC_Q6_BUG() \
-	if (get_restart_level() == RESET_SOC) \
-		BUG();
-#endif
